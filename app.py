c57a4a64fea48eeaa90d00a473fc8867bae9b092
Amirtharaj <amirtharajds@yahoo.com>
2023-05-16 13:35:47 +0530

    again

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ def index():
 
 @app.route("/play", methods=['GET', 'POST'])
 def play():
+    text = request.form.get('text')
+    converter = pyttsx3.init()
+    converter.setProperty('rate', 150)
+    converter.setProperty('volume', 1)
+    converter.say(text)
+    converter.runAndWait()
     return render_template("home.html")
 
 
