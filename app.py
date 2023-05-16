7d90818f9ce6773194ea193f047510f86bb327a6
Amirtharaj <amirtharajds@yahoo.com>
2023-05-16 13:33:11 +0530

    espeak

--- app.py
+++ app.py
@@ -11,12 +11,6 @@ def index():
 
 @app.route("/play", methods=['GET', 'POST'])
 def play():
-    text = request.form.get('text')
-    converter = pyttsx3.init()
-    converter.setProperty('rate', 150)
-    converter.setProperty('volume', 1)
-    converter.say(text)
-    converter.runAndWait()
     return render_template("home.html")
 
 
